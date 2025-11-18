7d120364d64a9d98b1f03e525ddc39ab90d31c97
chrisdutt24 <christian-duttlinger@naver.com>
2025-11-18 16:43:03 +0100

    removed all except todo

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,14 +1,9 @@
-import { Routes, Route, NavLink } from 'react-router-dom'
-import HomePage from './HomePage.jsx'
 import TodoPage from './TodoPage.jsx'
 
 function App() {
   return (
-      <main>
-        <Routes>
-          <Route path="/todos" element={<TodoPage />} />
-        </Routes>
-      </main>
+    <div style={{ padding: '2rem', fontFamily: 'system-ui' }}>
+      <TodoPage />
     </div>
   )
 }
